61b89a8631dae3c04bee557753697d2dfa32a528
ARPI SAHA <mailofarpisaha@gmail.com>
2025-03-08 18:50:19 +0530

    First Commit

---
+++ day2_let_var_const/script.js
@@ -0,0 +1,53 @@
+// using var
+// debugger;
+
+// var a = 5;
+// console.log("a = " + a);
+
+// var b;
+// b = 6;
+// console.log("b = " + b);
+// debugger;
+// console.log("before var c = " + vc);
+// var vc = 5;
+// console.log("after var c = " + vc);
+
+//using let
+
+// let a = 5;
+// console.log("a = " + a);
+
+// let b;
+// console.log("b = " + b);
+// b = 6;
+
+// console.log("before let c = " + lc);
+// let lc = 5;
+// console.log("after let c = " + lc);
+
+//using const
+// const a = 5;
+// console.log("a = " + a);
+
+// console.log("before const  c = " + c);
+// const c = 5;
+// console.log("after const  c = " + c);
+
+// debugger;
+// let greeting = "say Hi";
+// if (true) {
+//   let greeting = "say Hello instead";
+//   if (true) {
+//     let greeting = "say Bye instead";
+//     console.log(greeting);
+//   }
+//   console.log(greeting);
+// }
+// console.log(greeting);
+
+// debugger;
+// console.log(a);
+// let a;
+// console.log("a = " + a);
+// a = 5;
+// console.log("a = " + a);
