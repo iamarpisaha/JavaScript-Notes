231c487e949633f8e3d55f32c196ce385c0be5ca
ARPI SAHA <mailofarpisaha@gmail.com>
2025-03-13 19:47:15 +0530

    day9 added

---
+++ day9_function/script4.js
@@ -0,0 +1,20 @@
+// callback (passing a function as a parameter)
+
+// ##### passing a number as a parameter
+// function abc(age) {
+//   return age > 20;
+// }
+// let a = abc(22);
+// console.log(a);
+
+// here myFun function is a higher order function
+// here printHello function is a callback function
+
+// function myFun(callback) {
+//   console.log(callback());
+// }
+// function printHello() {
+//   console.log("hello");
+// }
+// // myFun(printHello());
+// myFun(printHello);
