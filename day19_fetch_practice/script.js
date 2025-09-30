26a6cad2963e85c89c4a11f1d80f6068e1a33e25
iamarpisaha <mailofarpisaha@gmail.com>
2025-09-30 17:21:57 +0530

    Day 19 updated

--- day19_fetch_practice/script.js
+++ day19_fetch_practice/script.js
@@ -1,30 +1,68 @@
-const apiBaseURL = "https://crud-api-4tam.onrender.com/api/students";
+const apiBaseURL = "https://crud-api-4tam.onrender.com/api";
 
-const studentsTableBody = document.getElementById("student-table-body");
+const studentForm = document.getElementById("student-form");
+
+studentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const payload = {
+    address: {},
+  };
+  for (const [key, value] of formData) {
+    if (["city", "state", "country", "pin"].includes(key)) {
+      payload["address"][key] = value;
+    } else {
+      payload[key] = value;
+    }
+  }
+
+  fetch(`${apiBaseURL}/students`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then(() => {
+      fetchStudents();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 function displayStudents(students) {
+  const studentsTableBody = document.getElementById("student-table-body");
   let studentsHTML = "";
   students.forEach((student) => {
-    console.log(student);
+    const { name, dob, gender, address } = student;
+    const { city, state, country, pin } = address || {};
     studentsHTML += `<tr>
-                  <td  class="px-6 py-3 text-left text-xs">
-                    ${student.name}
+                  <td  class="px-6 py-3 text-left">
+                    ${name}
                   </td>
-                  <td  class="px-6 py-3 text-left text-xs">
-                    ${student.dob.split("T")[0]}
+                  <td  class="px-6 py-3 text-left">
+                    ${dob.split("T")[0]}
                   </td>
-                  <td  class="px-6 py-3 text-left text-xs">
-                    ${student.gender}
+                  <td  class="px-6 py-3 text-left">
+                    ${gender}
                   </td>
-                  <td  class="px-6 py-3 text-left text-xs">
-                    ${student.address ? (student.address.city) : "-"}
+                  <td  class="px-6 py-3 text-left">
+                    ${city ? `${city}, ` : ""} 
+                    ${state ? `${state}` : ""}
+                    ${country ? `${country}, ` : ""}
+                    ${pin ? `${pin}` : ""}
                   </td>
                 </tr>`;
   });
   studentsTableBody.innerHTML = studentsHTML;
 }
+
 function fetchStudents() {
-  fetch(`${apiBaseURL}`)
+  fetch(`${apiBaseURL}/students`)
     .then((resp) => resp.json())
     .then((data) => {
       displayStudents(data);
@@ -33,4 +71,5 @@ function fetchStudents() {
       console.error(err);
     });
 }
+
 fetchStudents();
